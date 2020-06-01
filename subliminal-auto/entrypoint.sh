d07dbf1bf2c63f462d4874c4099c16362ce03a72
Ibraheem Alqwaiz <ialqwaiz@users.noreply.github.com>
2020-06-02 02:29:29 +0300

    Create entrypoint.sh

---
+++ subliminal-auto/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Start the run once job.
+echo "Docker container has been started"
+declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
+
+# Setup a cron schedule
+echo "SHELL=/bin/bash
+BASH_ENV=/container.env
+* * * * * . $HOME/.profile; python /subliminal_watchdog.py >> /tmp/log.txt 2>&1
+# This extra line makes it a valid cron" > crontab.txt
+
+crontab crontab.txt
+cron -f
